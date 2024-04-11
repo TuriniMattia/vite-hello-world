713b15f31940f7eb1456c3a0b1a59ffeb9dcf64f
TuriniMattia <turinimattia96@gmail.com>
2024-04-11 15:32:11 +0200

    first commit

---
+++ src/components/AppHero.vue
@@ -0,0 +1,27 @@
+<script setup>
+</script>
+
+<template>
+    <section class="bonus">
+        <img src="/public/esperienze-in-montagna-copertina.webp" alt="">
+        <h1 class="position">Titolo in mezzo</h1>
+    </section>
+
+
+</template>
+
+<style scoped>
+.bonus{
+    
+    position: relative;
+}
+.position{
+    color: rgb(237, 14, 59);
+    font-size: 50px;
+    position: absolute;
+    top: 50%;
+      left: 50%;
+      margin-right: -50%;
+      transform: translate(-50%, -50%);
+}
+</style>
